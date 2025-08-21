a484e0f8bec7523b3e7be2ad8d0efb85da7fcc2f
Omkar787-creator <omkar.pawar8180@gmail.com>
2025-08-21 10:13:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Show alert when someone clicks "Buy Now"
+function buyCar(carName) {
+  alert("Thank you for your interest in " + carName + 
+        "! Our sales team will contact you shortly.");
+}
